42f3a39f8c8f1d13f0288f45d1f44bdbd98533b9
Aditya Kumar Singh <cr7.aditya@gmail.com>
2020-06-09 01:09:04 +0530

    Done : Chapter-14

--- Chapter_08.sql
+++ Chapter_08.sql
@@ -5,31 +5,29 @@
  * @Last modified by:   the__martian
  */
 
-
-
------------------------------------------------------------
+-----------------------------------------------------------------
 --EXAMPLE
------------------------------------------------------------
+-----------------------------------------------------------------
 use SalesOrdersExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select p.ProductName, c.CategoryDescription
 from
     Products p
 inner join
     Categories c
 on p.CategoryID = c.CategoryID;
------------------------------------------------------------
+-----------------------------------------------------------------
 use EntertainmentAgencyExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select et.EntStageName as Name, eg.StartDate as 'Start Date', eg.EndDate as 'End Date', concat('$', eg.ContractPrice) as 'Contract Price'
 from
     Entertainers as et
 inner join
     Engagements as eg
 on et.EntertainerID  = eg.EntertainerID;
------------------------------------------------------------
+-----------------------------------------------------------------
 use SchoolSchedulingExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select s.SubjectName
 from
     Subjects as s
@@ -37,27 +35,27 @@ inner join
     Classes as c
 on s.SubjectID = c.SubjectID
 where WednesdaySchedule = -1;
------------------------------------------------------------
+-----------------------------------------------------------------
 use BowlingLeagueExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select t.TeamName as 'Team Name', concat(b.BowlerLastName, ', ', b.BowlerFirstName) as 'Captain Name'
 from
     Teams as t
 inner join
     Bowlers as b
 on t.CaptainID = b.BowlerID;
------------------------------------------------------------
+-----------------------------------------------------------------
 use RecipesExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 SELECT DISTINCT Recipes.RecipeTitle
 FROM Recipes
 INNER JOIN Recipe_Ingredients
   ON Recipes.RecipeID =
 Recipe_Ingredients.RecipeID
 WHERE Recipe_Ingredients.IngredientID IN (1, 9)
------------------------------------------------------------
+-----------------------------------------------------------------
 use SalesOrdersExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select DISTINCT concat(c.CustLastName, ', ', c.CustFirstName) as ' Customer'
 from Customers as c
 inner join
@@ -71,9 +69,9 @@ inner join
 on od.ProductNumber = p.ProductNumber
 where p.ProductName like '%Helmet%'
 order by  c.CustFirstName, c.CustLastName;
------------------------------------------------------------
+-----------------------------------------------------------------
 use EntertainmentAgencyExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select DISTINCT ent.EntStageName
 from Entertainers as ent
 inner join
@@ -83,9 +81,9 @@ inner join
     Customers as c
 on c.CustomerID = eg.CustomerID
 where c.CustFirstName like "%Berg%" or c.CustFirstName like "%Hallmark" or c.CustLastName like "%Berg%" or c.CustLastName like "%Hallmark%";
------------------------------------------------------------
+-----------------------------------------------------------------
 use BowlingLeagueExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select T.TourneyID as Tournament,  T.TourneyLocation as Location, TM.Lanes, TE.TeamName as 'Odd Lane Team', TEA.TeamName as 'Even Lane Team', winner.TeamName as 'Winning Team'
 from
     Tournaments as T
@@ -105,9 +103,9 @@ inner join
     Teams as winner
 on winner.TeamID  = MG.WinningTeamID
 order by T.TourneyID;
------------------------------------------------------------
+-----------------------------------------------------------------
 use RecipesExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select R.RecipeTitle as 'Recipe Title', I.IngredientName as 'Ingredient', RI.Amount as  'Amount', M.MeasurementDescription as Measurement
 from
 Recipes as R
@@ -120,9 +118,9 @@ on RI.IngredientID = I.IngredientID
 inner join
 Measurements as M
 on M.MeasureAmountID = RI.MeasureAmountID;
------------------------------------------------------------
+-----------------------------------------------------------------
 use SalesOrdersExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 (select C.CustFirstName as 'First Name', C.CustLastName as 'Last Name'
 from Customers as C
 inner join
@@ -148,9 +146,9 @@ inner join
 Products as p
 on p.ProductNumber = od.ProductNumber
 where p.ProductName like '%Helmet')
------------------------------------------------------------
+-----------------------------------------------------------------
 use EntertainmentAgencyExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 (select DISTINCT Ent.EntStageName as 'Entertainer'
 from
     Entertainers as Ent
@@ -172,18 +170,18 @@ from
     Customers as c
     on c.CustomerID = eg.CustomerID
 where c.CustLastName = 'Hallmark')
------------------------------------------------------------
+-----------------------------------------------------------------
 use SchoolSchedulingExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select concat(StudFirstName, ' ', StudLastName) as 'Student', concat(StfFirstName, ' ', StfLastname) as 'Teacher'
 from
     Students
     inner join
     Staff
     on StudFirstName = StfFirstName;
------------------------------------------------------------
+-----------------------------------------------------------------
 use BowlingLeagueExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select DISTINCT pla.Bowler
 from
 (select concat(B.BowlerFirstName, ' ', B.BowlerLastName) as 'Bowler', BS.MatchID as MatchID, BS.RawScore as score
@@ -207,7 +205,7 @@ where
 ) mat
 on mat.MatchID = pla.MatchID
 order by pla.Bowler;
------------------------------------------------------------
+-----------------------------------------------------------------
 select BowLan.Name as 'Full Name'
 from
 (
@@ -240,9 +238,9 @@ inner join
     where RawScore >= 170 and Tournaments.TourneyLocation = 'Bolero Lanes'
 ) as BowLan
 on BowLan.Name = BowThu.Name;
------------------------------------------------------------
+-----------------------------------------------------------------
 use RecipesExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select Re.RecipeID, Re.RecipeTitle, I.IngredientName
 from
 (
@@ -262,11 +260,11 @@ on RI.RecipeID = Re.RecipeID
 inner join
 Ingredients as I
 on I.IngredientID = RI.IngredientID;
------------------------------------------------------------
+-----------------------------------------------------------------
 --PROBLEMS
------------------------------------------------------------
+-----------------------------------------------------------------
 use SalesOrdersExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select concat(Customers.CustFirstName, ' ', Customers.CustLastName) as Customer, Orders.OrderDate as 'Order Date'
 from
 Customers
@@ -274,7 +272,7 @@ inner join
 Orders
 on Orders.CustomerID = Customers.CustomerID
 order by Orders.OrderDate;
------------------------------------------------------------
+-----------------------------------------------------------------
 select distinct concat(Customers.CustFirstName, ' ', Customers.CustLastName) as Customer, concat(Employees.EmpFirstName, ' ', Employees.EmpLastName) as Employee
 from
 Customers
@@ -285,7 +283,7 @@ inner join
 Employees
 on Employees.EmployeeID = Orders.EmployeeID;
 
------------------------------------------------------------
+-----------------------------------------------------------------
 select Orders.OrderNumber as 'Order Number', Orders.OrderDate as  'Order Date', P.ProductNumber as 'Product Number', P.ProductName as 'Name', OD.QuotedPrice as 'Price', OD.QuantityOrdered as 'Quantity', OD.QuotedPrice * OD.QuantityOrdered as 'Amount owed'
 from Orders
 inner join
@@ -294,7 +292,7 @@ on Orders.OrderNumber = OD.OrderNumber
 inner join
 Products as P
 on P.ProductNumber = OD.ProductNumber;
------------------------------------------------------------
+-----------------------------------------------------------------
 select Vendors.VendName, P.ProductName as Product, PV.WholesalePrice as 'Whole Sale Price'
 from Vendors
 inner join
@@ -304,30 +302,30 @@ inner join
 Products as P
 on P.ProductNumber = PV.ProductNumber
 where PV.WholesalePrice < 100;
------------------------------------------------------------
+-----------------------------------------------------------------
 select Customers.CustFirstName, Customers.CustLastName, Employees.EmpFirstName, Employees.EmpLastName
 from
     Customers
 inner join
     Employees
 on Customers.CustLastName = Employees.EmpLastName;
------------------------------------------------------------
+-----------------------------------------------------------------
 select Customers.CustFirstName, Customers.CustLastName, Employees.EmpFirstName, Employees.EmpLastName, Customers.CustCity as City
 from
     Customers
 inner join
     Employees
 on Customers.CustCity = Employees.EmpCity;
------------------------------------------------------------
+-----------------------------------------------------------------
 use EntertainmentAgencyExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select concat(A.AgtFirstName, ' ', A.AgtLastName) as Agent, Eg.StartDate as 'Start Date', Eg.EndDate as 'End Date'
 from Agents as A
 inner join
 Engagements as Eg
 on A.AgentID = Eg.AgentID
 order by Eg.StartDate;
------------------------------------------------------------
+-----------------------------------------------------------------
 select distinct concat(Customers.CustFirstName, ' ', Customers.CustLastName) as Customer, Entertainers.EntStageName as Entertainer
 from Customers
 inner join
@@ -336,22 +334,22 @@ on Customers.CustomerID = Engagements.CustomerID
 inner join
 Entertainers
 on Entertainers.EntertainerID = Engagements.EntertainerID;
------------------------------------------------------------
+-----------------------------------------------------------------
 select concat(A.AgtFirstName, ' ', A.AgtLastName) as Agent, EntStageName as Entertainer, A.AgtZipCode as 'Zip Code'
 from
 Agents as A
 inner join
 Entertainers
 on Entertainers.EntZipCode = A.AgtZipCode;
------------------------------------------------------------
+-----------------------------------------------------------------
 use SchoolSchedulingExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select B.BuildingName as 'Building', C.ClassRoomID
 from Buildings as B
 inner join
 Class_Rooms as C
 on C.BuildingCode = B.BuildingCode;
------------------------------------------------------------
+-----------------------------------------------------------------
 select distinct concat(S.StudFirstName, ' ', S.StudLastName) as Student, C.ClassID as Class, su.SubjectName as Subject
 from Students S
 inner join
@@ -364,7 +362,7 @@ inner join
 Subjects su
 on C.SubjectID = su.SubjectID;
 --??-- No of rows difffer
------------------------------------------------------------
+-----------------------------------------------------------------
 select concat(S.StfLastname, ' ', S.StfFirstName) as Staff, Sub.SubjectName as Subject
 from Staff S
 inner join
@@ -376,7 +374,7 @@ on FS.StaffID = F.StaffID
 inner join
 Subjects Sub
 on Sub.SubjectID = FS.SubjectID;
------------------------------------------------------------
+-----------------------------------------------------------------
 select distinct art.student
 from
 (select concat(st.StudLastName, ' ', st.StudFirstName) as student
@@ -407,33 +405,33 @@ on S.SubjectID = C.SubjectID
 where SubjectName like '%Computer%' and SS.Grade >= 85
 ) comp
 on art.student = comp.student;
------------------------------------------------------------
+-----------------------------------------------------------------
 use BowlingLeagueExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select Teams.TeamName as Team, concat(B.BowlerLastName, ', ', B.BowlerFirstName) as Bowler
 from Teams
 inner join
 Bowlers B
 on B.TeamID = Teams.TeamID;
------------------------------------------------------------
+-----------------------------------------------------------------
 ???
------------------------------------------------------------
+-----------------------------------------------------------------
 select concat(B.BowlerLastName, ', ', B.BowlerFirstName) as Bowler1, concat(b.BowlerLastName, ', ', b.BowlerFirstName) as Bowler2
 from Bowlers B
 inner join
 Bowlers b
 on b.BowlerZip =  B.BowlerZip
 where b.BowlerID <> B.BowlerID;
------------------------------------------------------------
+-----------------------------------------------------------------
 use RecipesExample;
------------------------------------------------------------
+-----------------------------------------------------------------
 select RecipeTitle
 from Recipes
 where RecipeTitle like '%Salad%';
------------------------------------------------------------
+-----------------------------------------------------------------
 ???
------------------------------------------------------------
+-----------------------------------------------------------------
 ???
------------------------------------------------------------
+-----------------------------------------------------------------
 ???
------------------------------------------------------------
+-----------------------------------------------------------------
--- Chapter_14.sql
+++ Chapter_14.sql
@@ -172,7 +172,7 @@ group by
     A.AgtLastName
 having
     sum(E.ContractPrice) > 3000;
------------------------------------------------------------------
+7
 use SchoolSchedulingExample;
 -----------------------------------------------------------------
 /*
@@ -181,3 +181,425 @@ name, the student name, and the student’s average grade of all
 classes taken in that category for those students who have an
 average higher than 90.”
 */
+select
+    C.CategoryDescription,
+    concat(S.StudFirstName, ' ', S.StudLastName) as Student,
+    avg(SS.Grade) as AvgGrade
+from
+    Students S
+inner join
+    Student_Schedules SS
+    on S.StudentID = SS.StudentID
+inner join
+    Student_Class_Status SCS
+    on SCS.ClassStatus = SS.ClassStatus
+inner join
+    Classes CL
+    on CL.ClassID = SS.ClassID
+inner join
+    Subjects SB
+    on SB.SubjectID = CL.SubjectID
+inner join
+    Categories C
+    on C.CategoryID = SB.CategoryID
+where
+    SCS.ClassStatusDescription = "Completed"
+group by
+    C.CategoryDescription,
+    S.StudFirstName,
+    S.StudLastName
+having
+    avg(SS.Grade) > 90;
+-----------------------------------------------------------------
+/*
+“List each staff member and the count of classes each is scheduled
+to teach for those staff members who teach at least one but fewer
+than three classes.”
+*/
+select
+    concat(S.StfFirstName, ' ', S.StfLastname) as Staff,
+    count(FC.ClassID) as NumberOfClass
+from
+    Staff S
+inner join
+    Faculty_Classes FC
+    on S.StaffID = FC.StaffID
+group by
+    S.StfFirstName,
+    S.StfLastname
+having
+    count(FC.ClassID) < 3;
+-----------------------------------------------------------------
+use BowlingLeagueExample;
+-----------------------------------------------------------------
+/*
+“List the bowlers whose highest raw scores are more than 20 pins
+higher than their current averages.”
+*/
+select
+    concat(B.BowlerFirstName, ' ', B.BowlerLastName) as Bowler,
+    round(avg(BS.RawScore), 0) as CurrentAverage,
+    max(BS.RawScore) as MaxRowScore
+from
+    Bowlers B
+inner join
+    Bowler_Scores BS
+    on B.BowlerID = BS.BowlerID
+group by
+    B.BowlerFirstName,
+    B.BowlerLastName
+having
+    max(BS.RawScore) > (round(avg(BS.RawScore), 0) + 20)
+order by
+    B.BowlerFirstName,
+    B.BowlerLastName;
+-----------------------------------------------------------------
+use RecipesExample;
+-----------------------------------------------------------------
+/*
+“List the recipes that contain both beef and garlic.”
+*/
+select
+    R.RecipeTitle
+from
+    Recipes R
+where
+    R.RecipeID in (
+        select
+            RI.RecipeID
+        from
+            Recipe_Ingredients RI
+        inner join
+            Ingredients I
+            on I.IngredientID = RI.IngredientID
+        where
+            I.IngredientName = 'Beef'
+    )
+    and
+    R.RecipeID in (
+        select
+            RI.RecipeID
+        from
+            Recipe_Ingredients RI
+        inner join
+            Ingredients I
+            on I.IngredientID = RI.IngredientID
+        where
+            I.IngredientName = 'Garlic'
+    );
+-----------------------------------------------------------------
+EXCERCISE QUESTION
+-----------------------------------------------------------------
+use SalesOrdersExample;
+-----------------------------------------------------------------
+/*
+“Show me each vendor and the average by vendor of the number of
+days to deliver products that are greater than the average
+delivery days for all vendors.”
+*/
+select
+    V.VendName,
+    avg(PV.DaysToDeliver) as AvgDayToDeliverProducts
+from
+    Vendors V
+inner join
+    Product_Vendors PV
+    on V.VendorID = PV.VendorID
+group by
+    V.VendName
+having
+    avg(PV.DaysToDeliver) > (
+        select
+            avg(pv.DaysToDeliver)
+        from
+            Product_Vendors pv
+    );
+-----------------------------------------------------------------
+/*
+“Display for each product the product name and the total sales
+that is greater than the average of sales for all products in
+that category.”
+*/
+????
+-----------------------------------------------------------------
+/*
+“How many orders are for only one product?”
+*/
+select
+    count(res.OrderNumber)
+from
+    (
+    select
+        OD.OrderNumber
+    from
+        Order_Details OD
+    group by
+        OD.OrderNumber
+    having
+        count(*) = 1;
+    ) as res;
+-----------------------------------------------------------------
+use EntertainmentAgencyExample;
+-----------------------------------------------------------------
+/*
+“Show me the entertainers who have more than two overlapped
+bookings.”
+*/
+select
+    Ent.EntertainerID,
+    Ent.EntStageName
+from
+    Entertainers Ent
+where
+    Ent.EntertainerID in
+    (
+        select
+            E1.EntertainerID
+        from
+            Engagements E1
+        inner join
+            Engagements E2
+            on E1.EntertainerID = E2.EntertainerID
+        where
+            E1.EngagementNumber <> E2.EngagementNumber
+            and
+            E1.StartDate <= E2.EndDate
+            and
+            E2.Startdate <= E1.EndDate
+        group by
+            E1.EntertainerID
+        order by
+            count(*) >= 2
+    );
+-----------------------------------------------------------------
+/*
+“Show each agent’s name, the sum of the contract price for the
+engagements booked, and the agent’s total commission for agents
+whose total commission is more than $1,000.”
+*/
+select
+    concat(A.AgtFirstName, ' ', A.AgtLastName) as Agent,
+    sum(E.ContractPrice) as TotalContractPrice,
+    sum(E.ContractPrice * A.CommissionRate) as TotalCommission
+from
+    Agents A
+inner join
+    Engagements E
+    on A.AgentID = E.AgentID
+group by
+    A.AgtFirstName,
+    A.AgtLastName
+having
+    sum(E.ContractPrice * A.CommissionRate) > 1000;
+-----------------------------------------------------------------
+use SchoolSchedulingExample;
+-----------------------------------------------------------------
+/*
+“Display by category the category name and the count of classes
+offered for those categories that have three or more classes.”
+*/
+select
+    C.CategoryDescription,
+    count(*) TotalClasses
+from
+    Categories C
+inner join
+    Subjects S
+    on C.CategoryID = S.CategoryID
+inner join
+    Classes CL
+    on CL.SubjectID = S.SubjectID
+group by
+    C.CategoryDescription
+having
+    count(*) >= 3;
+-----------------------------------------------------------------
+/*
+“List each staff member and the count of classes each is
+scheduled to teach for those staff members who teach fewer than
+three classes.”
+*/
+select
+    concat(S.StfFirstName, ' ', S.StfLastname) as Staff,
+    count(FC.ClassID) TotalClasses
+from
+    Staff S
+left join
+    Faculty_Classes as FC
+    on FC.StaffID = S.StaffID
+group by
+    S.StfFirstName,
+    S.StfLastname
+having
+    count(FC.ClassID) < 3;
+-----------------------------------------------------------------
+/*
+“Show me the subject categories that have fewer than three full
+professors teaching that subject.”
+*/
+select
+    C.CategoryDescription,
+    count(FCF.StaffID) as ProfCount
+from
+    Categories C
+left join
+    (
+        select
+            FC.CategoryID,
+            FC.StaffID
+        from
+            Faculty_Categories FC
+        inner join
+            Faculty F
+            on F.StaffID = FC.StaffID
+        where
+            F.Title = "Professor"
+    ) as FCF
+    on FCF.CategoryID = C.CategoryID
+group by
+    C.CategoryDescription
+having
+    count(FCF.StaffID) < 3;
+-----------------------------------------------------------------
+/*
+“Count the classes taught by every staff member.”
+*/
+select
+    concat(S.stfFirstName, ' ', S.stfLastName) as Staff,
+    count(FC.ClassID) as ClassCount
+from
+    Staff S
+left join
+    Faculty_Classes FC
+    on S.StaffID = FC.StaffID
+group by
+    S.stfFirstName,
+    S.StfLastname;
+-----------------------------------------------------------------
+use BowlingLeagueExample;
+-----------------------------------------------------------------
+/*
+“Do any team captains have a raw score that is higher than any
+other member of the team?”
+*/
+select
+    T.TeamID,
+    B.BowlerID,
+    concat(B.BowlerFirstName, ' ', B.BowlerLastName) as Captain,
+    max(BS.RawScore) as HighestRawScore
+from
+    Bowlers B
+inner join
+    Teams T
+    on B.BowlerID = T.CaptainID
+inner join
+    Bowler_Scores BS
+    on BS.BowlerID = B.BowlerID
+group by
+    T.TeamID,
+    B.BowlerID,
+    B.BowlerFirstName,
+    B.BowlerLastName
+having
+    max(BS.RawScore) > (
+        select
+            max(bs.RawScore)
+        from
+            Bowler_Scores bs
+        inner join
+            Bowlers b
+            on b.BowlerID = bs.BowlerID
+        where
+            bs.BowlerID <> B.BowlerID
+            and
+            b.TeamID = T.TeamID
+    );
+-----------------------------------------------------------------
+/*
+“Display for each bowler the bowler name and the average of the
+bowler’s raw game scores for bowlers whose average is greater
+than 155.”
+*/
+select
+    concat(B.BowlerFirstName, ' ', B.BowlerLastName) as Bowler,
+    avg(BS.RawScore) as AvgRawScore
+from
+    Bowlers B
+inner join
+    Bowler_Scores BS
+    on BS.BowlerID = B.BowlerID
+group by
+    B.BowlerFirstName,
+    B.BowlerLastName
+having
+    avg(BS.RawScore) > 155;
+-----------------------------------------------------------------
+/*
+“List the last name and first name of every bowler whose average
+raw score is greater than or equal to the overall average score.”
+*/
+select
+    B.BowlerLastName,
+    B.BowlerFirstName,
+    avg(BS.RawScore) as AvgRawScore
+from
+    Bowlers B
+inner join
+    Bowler_Scores BS
+    on BS.BowlerID = B.BowlerID
+group by
+    B.BowlerFirstName,
+    B.BowlerLastName
+having
+    avg(BS.RawScore) >= (
+        select
+            avg(bs.RawScore)
+        from
+            Bowler_Scores bs
+    );
+-----------------------------------------------------------------
+use RecipesExample;
+-----------------------------------------------------------------
+/*
+“Sum the amount of salt by recipe class, and display those
+recipe classes that require more than three teaspoons.”
+*/
+select
+    RC.RecipeClassDescription,
+    sum(RI.Amount)
+from
+    Recipe_Classes RC
+inner join
+    Recipes R
+    on RC.RecipeClassID = R.RecipeClassID
+inner join
+    Recipe_Ingredients as RI
+    on RI.RecipeID = R.RecipeID
+inner join
+    Ingredients I
+    on I.IngredientID = RI.IngredientID
+where
+    I.IngredientName = 'Salt'
+group by
+    RC.RecipeClassDescription
+having
+    sum(RI.Amount) > 3;
+-----------------------------------------------------------------
+/*
+“For what class of recipe do I have two or more recipes?”
+*/
+select
+    RC.RecipeClassDescription,
+    count(R.RecipeID)
+from
+    Recipe_Classes RC
+inner join
+    Recipes R
+    on RC.RecipeClassID = R.RecipeClassID
+group by
+    RC.RecipeClassDescription
+having
+    count(R.RecipeID) >= 2;
+-----------------------------------------------------------------
+                        T H E - E N D
+-----------------------------------------------------------------
