cecadffbed6891b2e609112ff2b9448bf2c149a5
Aditya Kumar Singh <cr7.aditya@gmail.com>
2020-06-30 14:44:28 +0530

    Chapter 18 completed

--- Chapter_18.sql
+++ Chapter_18.sql
@@ -978,6 +978,49 @@ where
 completed the prerequisite course.”
 */
 ???
+select distinct
+    S.StudentID,
+    S.StudFirstName,
+    S.StudLastName,
+    C.StartDate,
+    SB.SubjectCode,
+    SB.SubjectName,
+    SB.SubjectPreReq
+from
+    Students S
+inner join
+    Student_Schedules SS
+    on SS.StudentID = SS.StudentID
+inner join
+    Classes C
+    on C.ClassID = SS.ClassID
+inner join
+    Subjects SB
+    on SB.SubjectID = C.SubjectID
+where
+    SB.SubjectPreReq is not null
+    and
+    SB.SubjectPreReq not in (
+        select
+            SB2.SubjectCode
+        from
+            Subjects SB2
+        inner join
+            Classes C2
+            on C2.SubjectID = SB2.SubjectID
+        inner join
+            Student_Schedules SS2
+            on SS2.ClassID = C2.ClassID
+        inner join
+            Student_Class_Status SCS2
+            on SCS2.ClassStatus = SS2.ClassStatus
+        where
+            SCS2.ClassStatusDescription <> "Withdrew"
+            and
+            SS2.StudentID = S.StudentID
+            and
+            C2.StartDate <= C.StartDate
+    );
 -----------------------------------------------------------------
 use BowlingLeagueExample;
 -----------------------------------------------------------------
@@ -1456,13 +1499,632 @@ use EntertainmentAgencyExample;
  “List the entertainers who play the Jazz, Rhythm and Blues, and
  Salsa styles.”
  */
+select
+    E.EntertainerID,
+    E.EntStageName
+from
+    Entertainers E
+where
+    E.EntertainerID in  (
+        select
+            ES1.EntertainerID
+        from
+            Entertainer_Styles ES1
+        inner join
+            Musical_Styles MS1
+            on MS1.StyleID = ES1.StyleID
+        where
+            MS1.StyleName = 'Salsa'
+    )
+    and
+    E.EntertainerID in  (
+        select
+            ES2.EntertainerID
+        from
+            Entertainer_Styles ES2
+        inner join
+            Musical_Styles MS2
+            on MS2.StyleID = ES2.StyleID
+        where
+            MS2.StyleName = 'Jazz'
+    )
+    and
+    E.EntertainerID in  (
+        select
+            ES3.EntertainerID
+        from
+            Entertainer_Styles ES3
+        inner join
+            Musical_Styles MS3
+            on MS3.StyleID = ES3.StyleID
+        where
+            MS3.StyleName = 'Rhythm and Blues'
+    );
 -----------------------------------------------------------------
+/*
+“Show the entertainers who did not have a booking in the 90 days
+preceding May 1, 2018.”
+*/
+select
+    E.EntertainerID,
+    E.EntStageName
+from
+    Entertainers E
+where
+    E.EntertainerID not in (
+        select
+            EN.EntertainerID
+        from
+            Engagements EN
+        where
+            EN.StartDate >
+            date_add('2018-05-01', INTERVAL -90 DAY)
+    );
 -----------------------------------------------------------------
+/*
+“Display the customers who have not booked Topazz or Modern
+Dance.”
+*/
+select
+    C.CustomerID,
+    C.CustFirstName,
+    C.CustLastName
+from
+    Customers C
+where
+    C.CustomerID not in
+    (
+        select
+            E1.CustomerID
+        from
+            Engagements E1
+        inner join
+            Entertainers EN1
+            on E1.EntertainerID = EN1.EntertainerID
+        where
+            EN1.EntStageName = 'Topazz'
+    )
+    and
+    C.CustomerID not in
+    (
+        select
+            E2.CustomerID
+        from
+            Engagements E2
+        inner join
+            Entertainers EN2
+            on E2.EntertainerID = EN2.EntertainerID
+        where
+            EN2.EntStageName = 'Modern Dance'
+    );
 -----------------------------------------------------------------
+/*
+“List the entertainers who have performed for Hartwig, McCrae,
+and Rosales.”
+*/
+select
+    E.EntertainerID,
+    E.EntStageName
+from
+    Entertainers E
+where
+    E.EntertainerID in (
+        select
+            EN1.EntertainerID
+        from
+            Engagements EN1
+        inner join
+            Customers C1
+            on C1.CustomerID = EN1.CustomerID
+        where
+            C1.CustLastName = 'Hartwig'
+    )
+    and
+    E.EntertainerID in (
+        select
+            EN2.EntertainerID
+        from
+            Engagements EN2
+        inner join
+            Customers C2
+            on C2.CustomerID = EN2.CustomerID
+        where
+            C2.CustLastName = 'McCrae'
+    )
+    and
+    E.EntertainerID in (
+        select
+            EN3.EntertainerID
+        from
+            Engagements EN3
+        inner join
+            Customers C3
+            on C3.CustomerID = EN3.CustomerID
+        where
+            C3.CustLastName = 'Rosales'
+    );
 -----------------------------------------------------------------
+/*
+“Display the customers who have never booked an entertainer.”
+*/
+select
+    C.CustomerID,
+    C.CustFirstName,
+    C.CustLastName
+from
+    Customers C
+where
+    C.CustomerID not in (
+        select
+            E.CustomerID
+        from
+            Engagements E
+    );
 -----------------------------------------------------------------
+/*
+“Show the entertainers who have no bookings.”
+*/
+select
+    E.EntertainerID,
+    E.EntStageName
+from
+    Entertainers E
+where
+    E.EntertainerID not in (
+        select
+            EN.EntertainerID
+        from
+            Engagements EN
+    );
 -----------------------------------------------------------------
+use SchoolSchedulingExample;
 -----------------------------------------------------------------
+/*
+“Show students who have a grade of 85 or better in both Art and
+Computer Science.”
+*/
+select
+    S.StudentID,
+    S.StudFirstName,
+    S.StudLastName
+from
+    Students S
+where
+    exists (
+        select
+            *
+        from
+            Student_Schedules SS1
+        inner join
+            Classes C1
+            on C1.ClassID = SS1.ClassID
+        inner join
+            Subjects S1
+            on S1.SubjectID = C1.SubjectID
+        inner join
+            Categories CA1
+            on CA1.CategoryID = S1.CategoryID
+        where
+            CA1.CategoryDescription = 'Art'
+            and
+            SS1.Grade >= 85
+            and
+            S.StudentID = SS1.StudentID
+    )
+    and
+    exists (
+    select
+        *
+    from
+        Student_Schedules SS2
+    inner join
+        Classes C2
+        on C2.ClassID = SS2.ClassID
+    inner join
+        Subjects S2
+        on S2.SubjectID = C2.SubjectID
+    inner join
+        Categories CA2
+        on CA2.CategoryID = S2.CategoryID
+    where
+        CA2.CategoryDescription like '%Computer%'
+        and
+        SS2.Grade >= 85
+        and
+        SS2.StudentID = S.StudentID
+    );
 -----------------------------------------------------------------
+/*
+“Display the staff members who are teaching classes for which
+they are not accredited.”
+*/
+select
+    S.StaffID,
+    S.StfFirstName,
+    S.StfLastname
+from
+    Staff S
+inner join
+    Faculty_Classes FC
+    on S.StaffID = FC.StaffID
+inner join
+    Classes C
+    on C.ClassID = FC.ClassID
+inner join
+    Subjects SB
+    on SB.SubjectID = C.SubjectID
+where
+    SB.SubjectID not in  (
+        select
+            FS.SubjectID
+        from
+            Faculty_Subjects FS
+        where
+            FS.StaffID = S.StaffID
+    );
 -----------------------------------------------------------------
+/*
+“List the students who have passed all completed classes with a
+grade of 80 or better.”
+*/
+select
+    S.StudentID,
+    S.StudFirstName,
+    S.StudLastName
+from
+    Students S
+where
+    80 <= (
+        select
+            min(SS.Grade)
+        from
+            Student_Schedules SS
+        inner join
+            Student_Class_Status SCS
+            on SCS.ClassStatus = SS.ClassStatus
+        where
+            SCS.ClassStatusDescription = "Completed"
+            and
+            SS.StudentID = S.StudentID
+    );
+-----------------------------------------------------------------
+/*
+“Find classes with no students.”
+*/
+select
+    C.ClassID
+from
+    Classes C
+where
+    C.ClassID not in (
+        select
+            SS.ClassID
+        from
+            Student_Schedules SS
+        inner join
+            Students S
+            on S.StudentID = SS.StudentID
+        inner join
+            Student_Class_Status SCS
+            on SCS.ClassStatus = SS.ClassStatus
+        where
+            SCS.ClassStatusDescription = "Enrolled"
+    );
+-----------------------------------------------------------------
+/*
+“Show which students have never withdrawn.”
+*/
+select
+    concat(S.StudFirstName, ' ', S.StudLastName) FullName
+from
+    Students S
+where
+    S.StudentID not in (
+        select
+            SS.StudentID
+        from
+            Student_Schedules SS
+        inner join
+            Student_Class_Status SCS
+            on SCS.ClassStatus = SS.ClassStatus
+        where
+            SCS.ClassStatusDescription = "Withdrew"
+    );
+-----------------------------------------------------------------
+/*
+“Find subjects that have no faculty assigned.”
+*/
+select
+    S.SubjectID,
+    S.SubjectName
+from
+    Subjects S
+left join
+    Faculty_Subjects FS
+    on FS.SubjectID = S.SubjectID
+where
+    FS.SubjectID is null;
+-----------------------------------------------------------------
+/*
+“List students not currently enrolled.”
+*/
+select
+    S.StudentID,
+    concat(S.StudFirstName, ' ', S.StudLastName) FullName
+from
+    Students S
+where
+    S.StudentID not in (
+        select
+            SS.StudentID
+        from
+            Student_Schedules SS
+        inner join
+            Student_Class_Status SCS
+            on SCS.ClassStatus = SS.ClassStatus
+            where
+                SCS.ClassStatusDescription = "Enrolled"
+
+    );
+-----------------------------------------------------------------
+/*
+“Display staff members not teaching.”
+*/
+select
+    S.StaffID,
+    S.StfFirstName,
+    S.StfLastname
+from
+    Staff S
+where
+    S.StaffID not in (
+        select
+            FC.StaffID
+        from
+            Faculty_Classes FC
+    );
+-----------------------------------------------------------------
+use BowlingLeagueExample;
+-----------------------------------------------------------------
+/*
+“Display the bowlers who have never bowled a raw score greater
+than 150.”
+*/
+select
+    B.BowlerID,
+    B.BowlerFirstName,
+    B.BowlerLastName
+from
+    Bowlers B
+where
+    not exists (
+        select
+            *
+        from
+            Bowler_Scores BS
+        where
+            BS.BowlerID = B.BowlerID
+            and
+            BS.RawScore > 150
+    );
+/*
+using in
+*/
+select
+    B.BowlerID,
+    B.BowlerFirstName,
+    B.BowlerLastName
+from
+    Bowlers B
+where
+    B.BowlerID not in (
+        select
+            BS.BowlerID
+        from
+            Bowler_Scores BS
+        group by
+            BS.BowlerID
+        having
+            max(BS.RawScore) > 150
+    );
+-----------------------------------------------------------------
+/*
+“Show the bowlers who have a raw score greater than 170 at both
+Thunderbird Lanes and Bolero Lanes.”
+*/
+select
+    B.BowlerID,
+    B.BowlerFirstName,
+    B.BowlerLastName
+from
+    Bowlers B
+where
+    exists (
+        select
+            BS1.BowlerID
+        from
+            Bowler_Scores BS1
+        inner join
+            Tourney_Matches TM1
+            on TM1.MatchID = BS1.MatchID
+        inner join
+            Tournaments T1
+            on T1.TourneyID = TM1.TourneyID
+        where
+            BS1.RawScore >= 170
+            and
+            T1.TourneyLocation = "Thunderbird Lanes"
+            and
+            BS1.BowlerID = B.BowlerID
+    )
+    and
+    exists (
+        select
+            BS2.BowlerID
+        from
+            Bowler_Scores BS2
+        inner join
+            Tourney_Matches TM2
+            on TM2.MatchID = BS2.MatchID
+        inner join
+            Tournaments T2
+            on T2.TourneyID = TM2.TourneyID
+        where
+            BS2.RawScore >= 170
+            and
+            T2.TourneyLocation = "Bolero Lanes"
+            and
+            BS2.BowlerID = B.BowlerID
+    );
+-----------------------------------------------------------------
+/*
+“List the tournaments that have not yet been played.”
+*/
+select
+    T.TourneyID,
+    T.TourneyDate
+from
+    Tournaments T
+where
+    T.TourneyID not in (
+        select
+            TM.TourneyID
+        from
+            Tourney_Matches TM
+    );
+-----------------------------------------------------------------
+use RecipesExample;
+-----------------------------------------------------------------
+/*
+“Show me the recipes that have beef and garlic.”
+*/
+select
+    R.RecipeID,
+    R.RecipeTitle
+from
+    Recipes R
+where
+    R.RecipeID in (
+        select
+            RI1.RecipeID
+        from
+            Recipe_Ingredients RI1
+        inner join
+            Ingredients I1
+            on I1.IngredientID = RI1.IngredientID
+        where
+            I1.IngredientName = "Garlic"
+    )
+    and
+    R.RecipeID in (
+        select
+            RI2.RecipeID
+        from
+            Recipe_Ingredients RI2
+        inner join
+            Ingredients I2
+            on I2.IngredientID = RI2.IngredientID
+        where
+            I2.IngredientName = "Beef"
+    );
+-----------------------------------------------------------------
+/*
+“List the recipes that have beef, onion, and carrot.”
+*/
+select
+    R.RecipeID,
+    R.RecipeTitle
+from
+    Recipes R
+where
+    R.RecipeID in (
+        select
+            RI1.RecipeID
+        from
+            Recipe_Ingredients RI1
+        inner join
+            Ingredients I1
+            on I1.IngredientID = RI1.IngredientID
+        where
+            I1.IngredientName = "Onion"
+    )
+    and
+    R.RecipeID in (
+        select
+            RI2.RecipeID
+        from
+            Recipe_Ingredients RI2
+        inner join
+            Ingredients I2
+            on I2.IngredientID = RI2.IngredientID
+        where
+            I2.IngredientName = "Beef"
+    )
+    and
+    R.RecipeID in (
+        select
+            RI3.RecipeID
+        from
+            Recipe_Ingredients RI3
+        inner join
+            Ingredients I3
+            on I3.IngredientID = RI3.IngredientID
+        where
+            I3.IngredientName = "Carrot"
+    );
+-----------------------------------------------------------------
+/*
+“Which recipes use no dairy products (cheese, butter, dairy)?”
+*/
+select
+    R.RecipeID,
+    R.RecipeTitle
+from
+    Recipes R
+where
+    R.RecipeID not in (
+        select
+            RI1.RecipeID
+        from
+            Recipe_Ingredients RI1
+        inner join
+            Ingredients I1
+            on I1.IngredientID = RI1.IngredientID
+        where
+            I1.IngredientName in ("Cheese", "Butter", "Dairy")
+    );
+-----------------------------------------------------------------
+/*
+“Display ingredients not used in any recipe.”
+*/
+select
+    I.IngredientID,
+    I.IngredientName
+from
+    Ingredients I
+where
+    I.IngredientID not  in (
+        select
+            RI.IngredientID
+        from
+            Recipe_Ingredients RI
+    );
+-----------------------------------------------------------------
+/*
+“Show recipe classes for which there is no recipe.”
+*/
+select
+    RC.RecipeClassID,
+    RC.RecipeClassDescription
+from
+    Recipe_Classes RC
+where
+    RC.RecipeClassID not in (
+        select
+            R.RecipeClassID
+        from
+            Recipes R
+    );
+-----------------------------------------------------------------
+                    T H E - E N D
 -----------------------------------------------------------------
