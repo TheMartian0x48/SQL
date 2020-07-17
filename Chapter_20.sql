e68575f112aad4885b3dbf37cc44216536f456c4
Aditya Kumar Singh <cr7.aditya.cs@gmail.com>
2020-07-18 00:20:08 +0530

    Started chapter 20

---
+++ Chapter_20.sql
@@ -0,0 +1,9 @@
+/**
+ * @Author: ADITYA KUMAR SINGH <the__martian>
+ * @Email:  cr7.aditya.cs@gmail.com
+ * @Filename: Chapter_20.sql
+ */
+
+-----------------------------------------------------------------
+              I N T E R N A L  P R O B L E M S
+-----------------------------------------------------------------
